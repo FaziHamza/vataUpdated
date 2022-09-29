9d3b73bc615a5f999a100b3b4133e667ac8095a5
faziHamza <faizan8887r@gmail.com>
2022-09-29 20:45:28 +0500

    d

---
+++ src/app/pages/components/dashboard/desktop-dashboard/booking-app/booking-app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-app',
+  templateUrl: './booking-app.component.html',
+  styleUrls: ['./booking-app.component.scss']
+})
+export class BookingAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
